e6edc51dc4a97b1363014a8e2a496ba1d605ea7d
Tigran Saakyan <38560578+tigraqwerty@users.noreply.github.com>
2021-08-03 23:24:16 +0200

    Release/1.0.0 (#1)
    * ADD: add initial packag.json config
    
    * ADD: Dependecy configuration
    
    * ADD: new dependency to app
    
    * ADD: simple chat tempalte
    
    * ADD: implementation of imaage reption and end proccess
    
    * ADD: recuperation form backup flie

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import {v4 as uuid} from 'uuid';
+import { writeFile, existsSync, readFileSync } from 'fs';
+
+const wss = new WebSocketServer({port: '8088'});
+
+const logs = existsSync('./logs/log.json') && readFileSync('./logs/log.json');
+
+const clients = {};
+const messages =  JSON.parse(logs) || [];
+
+wss.on('connection', (ws) => {
+	const id = uuid(); 
+	clients[id] = ws;
+	console.info(`SERVER: client with id "${id}" added`);
+
+	ws.send(JSON.stringify(messages))
+
+	ws.on('message', (newMessage) => {
+		let {name, message} = JSON.parse(newMessage);
+		messages.push({name, message})
+
+		for(let clientId in clients) {
+		clients[clientId].send(JSON.stringify([{name, message}]))
+		}
+	});
+
+	ws.on('close', () => {
+		delete clients[id];
+		console.info(`SERVER: client with "${id}" deleted`)
+	});
+});
+
+process.on('SIGINT', () => {
+	wss.close();
+	writeFile('./logs/log.json', JSON.stringify(messages), err => {
+		if(err) {
+			console.log(err)
+		}
+		process.exit()
+	})
+	
+})
